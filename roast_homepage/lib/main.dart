fdc38a13d591b7c678f937cd88be46878293808f
Ahad Sheriff <ahadsheriff@gmail.com>
2020-04-19 13:54:53 -0700

    added scaffolding

--- roast_homepage/lib/main.dart
+++ roast_homepage/lib/main.dart
@@ -1,9 +1,21 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MyApp(
-        Scaffold(
-          backgroundColor: ,
+void main() { runApp(
+      MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.redAccent,
+          // appBar: AppBar(
+          //   title: Text('Roast'),
+          //   backgroundColor: Colors.redAccent,
+          // ),
+          body: 
+          Center(
+            child: Image(
+              image: AssetImage('images/fire.gif'),
+            ),
+          ),
         ),
       ),
     );
+}
